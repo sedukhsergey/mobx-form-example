85487bd5b4b16b8b1da2aeaba6a770c640738989
sedukh <sedukhsergey@gmail.com>
2020-05-24 11:53:41 +0300

    add eslnt

--- src/modules/WishList/WishListItemView/WishListItemView.js
+++ src/modules/WishList/WishListItemView/WishListItemView.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.css';
 import { clone, getSnapshot, applySnapshot } from 'mobx-state-tree';
 import { WishListItemEdit } from '../../index';
